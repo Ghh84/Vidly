3fc48146b77f9f4a0d13bb43f25646ab720a2706
Ghh84 <74573748+Ghh84@users.noreply.github.com>
2022-03-30 10:39:23 +0300

    Add files via upload

---
+++ src/utilts/paginate.js
@@ -0,0 +1,5 @@
+import _ from 'lodash'
+ export function paginate(items, pageNumber,pageSize){
+     const startIndex=(pageNumber-1)* pageSize
+     return(_(items).slice(startIndex).take(pageSize).value())
+ }
